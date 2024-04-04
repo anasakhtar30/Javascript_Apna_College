0a61169ccf5e2a6df15e9fecd7c8671949e6fc90
Anas Akhter <anasmallick8757@gmail.com>
2024-04-05 01:05:51 +0530

    Variable_and_data_type

---
+++ script-1.js
@@ -0,0 +1,105 @@
+// console.log("Welcom to Apna College with JS");
+// console.log("Anas Akhter");
+// console.log("I love JS");
+
+
+
+// fullName = "Tony Stark";
+// console.log(fullName);
+
+// age = 34;
+// console.log(age);
+
+// price = 99;
+// console.log(price);
+
+// radius = 13;
+// console.log(radius);
+
+
+
+// x = null;
+// y = undefined;
+// console.log(x);
+// console.log(y);
+
+// isfollow = true;
+// console.log(isfollow);
+
+// fullname = "Anas mallick";
+// fullName = "Ankita Koley";
+
+
+// console.log(fullname)
+// console.log(fullName)
+
+
+
+// totalPrice = 98;
+// console.log(totalPrice);
+
+// let fullName = "Tony Stark";
+// let age = 23;
+// let totalPrice = 54;
+// console.log(age);
+
+
+// {
+//     let a= 9;
+//     console.log(a);
+// }
+
+// {
+//     let a = 18;
+//     console.log(a);
+// }
+
+// let age = 43;
+// let isFollow = false;
+// let fullName = "Tony Stark";
+// let x = null;
+// let y = undefined;
+
+// const student = {
+//     fullName : "rahul Kumar",
+//     age : 34,
+//     cgpa : 9.3,
+//     marks: 34,
+//     isPass :true
+// }
+
+// student.age = student.age + 1;
+// student.fullName = "Rahul Sharma";
+// console.log(student.fullName);
+// console.log(student["age"]);
+// console.log(student.age);
+
+
+//Let's Practice Example - 1
+
+// const product = {
+//     title:"Ball Pen",
+//     rating:4,
+//     offer:5,
+//     price:270
+// };
+// product.title = "Gel Pen";
+// product.rating = 5;
+
+// console.log(product["offer"]);
+
+//Let's Practice - Example -2 
+
+
+/*const profile = {
+    fullName:"shradha Khapra",
+    isFollow:true,
+    post:195,
+    follower:569,
+    following:4,
+    email:"@shradhakhapra",
+};
+
+console.log(typeof profile.isFollow);
+*/
+
